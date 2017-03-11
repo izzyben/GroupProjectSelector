8824f619da9a4597dd4513fe29ddde984ffc0a84
Izzyben <isilbeno7@gmail.com>
2017-03-11 16:22:54 +0100

    initial

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-include("connection.php"); // Establishing connection with our database
+include("dbconnect.php"); // Establishing connection with our database
 if(empty($_POST[ "username"]) || empty($_POST[ "password"]))
  {
      echo "Both fields are required. ";

13d56bfe07935aa8ae92b7629c3b964ef6ad5122
Izzy Ben <isilbeno7@gmail.com>
2017-04-10 22:20:51 +0100

    Initial

--- examples/index1.php
+++ examples/index1.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <h1>Login</h1><br><br>
-<form action="login.php" method="post">
+<form action="../login.php" method="post">
     <input type="text" name="username" placeholder="Username" /><br><br>
     <input type="password" name="password" placeholder="Password" /><br><br>
     <input type="submit" name="submit" value="Log In" />

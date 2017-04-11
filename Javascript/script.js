0579fc875b371ffd55c072b1341d8627e22fed24
Izzy Ben <isilbeno7@gmail.com>
2017-04-11 15:35:49 +0100

    Initial

--- Javascript/script.js
+++ Javascript/script.js
@@ -8,14 +8,14 @@ carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName("Slides");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}
     x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 5000);
+    setTimeout(carousel, 5000); // Change image every 5 seconds
 }
 
 

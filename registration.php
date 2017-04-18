2c88cdef19c506c16c53a92360c7fa991bfe9068
Izzy Ben <isilbeno7@gmail.com>
2017-04-18 14:42:19 +0100

    Initial

--- registration.php
+++ registration.php
@@ -5,3 +5,9 @@
  * Date: 13/04/2017
  * Time: 06:01
  */
+
+include("dbconnect.php"); // Establishing connection with our database
+
+if(empty($_POST[ 'usn'])){
+    echo "Both fields are required.";
+}

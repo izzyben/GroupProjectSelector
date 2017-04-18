43af588e75d15796e2d9705550f4998d37e4a660
Izzy Ben <isilbeno7@gmail.com>
2017-04-18 14:26:32 +0100

    Initial

--- login.php
+++ login.php
@@ -17,13 +17,13 @@ $result = mysqli_query($db,$sql);
 
     if( mysqli_num_rows($result) == 1)
         {
-     //$_SESSION['username'] = $username;
-     //if ($username == 'admin'){
-         //header("location: adminprofile.html");
- //}
-     //else{
+     $_SESSION['username'] = $username;
+      if($username == 'admin'){
+         header("location: adminprofile.html");
+ }
+     else{
        header("location: studentprofile.html"); // Redirecting To Student Profile Page
-    // }
+     }
         }
  else
  {

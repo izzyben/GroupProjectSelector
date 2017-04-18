1a45dbf895a9e55f04ae27a31678bf96007541c7
Izzy Ben <isilbeno7@gmail.com>
2017-04-18 14:24:25 +0100

    Initial

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if(empty($_POST[ 'username']) || empty($_POST[ 'password']))
      $username=$_POST["username"];
      $password=$_POST["password"];
 
-$sql = "SELECT user_id FROM users WHERE username='$username' OR email='$username' and password='$password';";
+$sql = "SELECT user_id FROM users WHERE (username='$username' OR email='$username') and password='$password';";
 $result = mysqli_query($db,$sql);
 
     if( mysqli_num_rows($result) == 1)

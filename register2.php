77ce915f1bd32fd17bfc800446cf2e63cd1b45f7
Izzy Ben <isilbeno7@gmail.com>
2017-04-19 15:16:15 +0100

    Initial

---
+++ register2.php
@@ -0,0 +1,24 @@
+<?php
+
+include ("dbconnect.php");
+
+if (isset($_POST['signup1']))
+{
+    $username = $_POST['usn'];
+    $email = $_POST['email'];
+    $password = $_POST['psw'];
+    $first_name = $_POST['first'];
+    $last_name = $_POST['last'];
+
+    $sql = "INSERT INTO users ('Firstname','Lastname','username','password','email') VALUES ('$first_name','$last_name','$username','$password','$email');";
+
+    $result = mysqli_query($db, $sql);
+    if ($result) {
+        echo "Success!! Login";
+        header("location: index.html"); // Redirecting to Home Page
+    }
+    else {
+        echo "Error!";
+        echo "<script> window.location.reload()</script>";
+    }
+}

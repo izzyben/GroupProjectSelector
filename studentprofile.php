ab9e726f2f51fad1de1e895a5a45021d474bc5d4
Izzy Ben <isilbeno7@gmail.com>
2017-04-19 18:43:11 +0100

    Initial

--- studentprofile.php
+++ studentprofile.php
@@ -66,17 +66,19 @@ $username = $_SESSION['username'];
     <div class="demo-drawer mdl-layout__drawer mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
         <header class="demo-drawer-header">
             <img src="images/user.jpg" class="demo-avatar">
-            <div class="demo-avatar-dropdown">
+            <section class="demo-avatar-dropdown">
                 <span><?php echo $username;?><p>Student</p></span>
                 <div class="mdl-layout-spacer"></div>
                 <button id="accbtn" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
                     <i class="material-icons" role="presentation">arrow_drop_down</i>
                     <span class="visuallyhidden">Accounts</span>
                 </button>
+                <section>
                 <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="accbtn">
-                    <li class="mdl-menu__item">Change Profile Picture</li>
+                    <li class="mdl-menu__item"><a href="">Change Profile Picture</a></li>
                 </ul>
-            </div>
+                </section>
+            </section>
         </header>
         <div class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
             <button class="mdl-navigation__link tablinks"  href="" onclick="openMenu(event,'Hme')"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i><b style="color: #0d47a1">Home</b></button>

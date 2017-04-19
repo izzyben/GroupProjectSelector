60490de4c348b5b730592bf4be9006600e7b7f71
Izzy Ben <isilbeno7@gmail.com>
2017-04-19 16:42:24 +0100

    Initial

--- register2.php
+++ register2.php
@@ -10,20 +10,29 @@ if (isset($_POST['signup1']))
     $first_name = $_POST['first'];
     $last_name = $_POST['last'];
 
-    $sql = "INSERT INTO users (Firstname,Lastname,username,password,email) VALUES ('$first_name','$last_name','$username','$password','$email');";
 
-    $result = mysqli_query($db, $sql);
-    if ($result) {
-        echo "<script language=\"JavaScript\">\n";
-        echo "alert('Registration Successful!');\n";
-        echo "window.location='index.html'";
-        echo "</script>";
-        header("location: index.html"); // Redirecting to Home Page
+    $user_check = $link->query("SELECT * FROM users WHERE username = '" . $username . "' OR email = '" . $email . "'");
+
+    if ($user_check->num_rows == 1) {
+
+        echo "<script language='JavaScript'> alert('User ALready Exists'); </script>";
     }
     else {
-        echo "<script language=\"JavaScript\">\n";
-        echo "alert('Username or Password was incorrect!');\n";
-        echo "window.location='index.html'";
-        echo "</script>";
+
+        $sql = "INSERT INTO users (Firstname,Lastname,username,password,email) VALUES ('$first_name','$last_name','$username','$password','$email');";
+
+        $result = mysqli_query($db, $sql);
+        if ($result) {
+            echo "<script language=\"JavaScript\">\n";
+            echo "alert('Registration Successful!');\n";
+            echo "window.location='index.html'";
+            echo "</script>";
+            header("location: index.html"); // Redirecting to Home Page
+        } else {
+            echo "<script language=\"JavaScript\">\n";
+            echo "alert('Username or Password was incorrect!');\n";
+            echo "window.location='index.html'";
+            echo "</script>";
+        }
     }
 }

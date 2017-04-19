a20a71a404bca79a4cdfbee516885758ea4acea0
Izzy Ben <isilbeno7@gmail.com>
2017-04-19 17:30:03 +0100

    Initial

--- registration.php
+++ registration.php
@@ -34,10 +34,12 @@ if (isset($_POST['signup'])) {
 
         $user_check = $db->query("SELECT * FROM users WHERE username = '" . $username . "' OR email = '" . $username . "'");
 
-        if ($user_check->num_rows == 1) {
+        if ($user_check->num_rows != 0) {
 
-            echo "<script language='JavaScript'> alert('Username/Email Address has been Registered!! Login or Register with another Username/Email'); </script>";
-            header("location: index.html"); // Redirecting to Home Page
+            echo "<script language=\"JavaScript\">\n";
+            echo "alert('Username/Email Address has been Registered!! Login or Register with another Username/Email');\n";
+            echo "window.location='index.html'";
+            echo "</script>";
 
         } else {
             $sql = "INSERT INTO users ('Firstname','Lastname','username','password','email') VALUES ('$first_name','$last_name','$username','$password','$email');";
